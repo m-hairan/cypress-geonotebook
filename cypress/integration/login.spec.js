c7b0eea6140a724dea0abe9b196cf53262e06b0e
Unitech <51434117+unitech1120@users.noreply.github.com>
2020-01-08 10:04:22 +0100

    Update login.spec.js

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,7 +1,7 @@
 describe('Login', () => {
   it('Login Action', () => {
     cy.visit('/')
-    cy.url().should('be.contain',  'login').then(async (url) => {
+    cy.url().should('be.contain',  'qweqweqeqw').then(async (url) => {
       cy.log('Page loaded')
       cy.login()
     })
